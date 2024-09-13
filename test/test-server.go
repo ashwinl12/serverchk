9c1652d86eb67af18d7abc95cdc32a01b85dc5ae
Ashwini Kumar <ashwini.kumar@bigohtech.com>
2024-09-13 15:37:22 +0530

    feature: Added ServerStat func

--- test/test-server.go
+++ test/test-server.go
@@ -5,3 +5,7 @@ const serverAliveMessage string = "Server is alive and kickin' !"
 func ServerAlive() string {
 	return serverAliveMessage
 }
+
+func ServerStats() string {
+	return "Server alive since 9 hours"
+}
